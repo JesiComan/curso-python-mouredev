eea15d718a6ae7a6d049ad7b0ef518444f70e122
JesiComan <jesicabelen.coman@gmail.com>
2024-02-09 14:00:29 -0300

    clase dicts

--- 06_sets.py
+++ 06_sets.py
@@ -25,6 +25,37 @@ print("Comancho" in my_other_set)
 my_other_set.remove("Coman")
 print(my_other_set)
 
+my_other_set.clear() #El clear elimina
+print(my_other_set)
+print(len(my_other_set))
+
+del my_other_set
+#print(my_other_set)
+
+my_set = {"Jesi", "Coman", 27}
+my_list = list(my_set)
+print(my_list)
+
+my_other_set= {"Tomate", "Hamburguesa", "Queso", "Lechuga", "Cebolla", 3}
+
+
+my_new_set = my_set.union(my_other_set)
+print(my_new_set)
+
+my_new_set = {"Tomate", "Hamburguesa", "Queso", "Lechuga", "Cebolla", 3}
+my_list = list(my_set)
+print(my_list)
+print(my_list[0])
+
+my_other_list = {"Tomate", "Hamburguesa", "Queso", "Lechuga", "Cebolla", 3}
+my_new_set = my_set.union(my_other_set)
+print(my_new_set.union(my_other_set))
+print(my_new_set.union(my_new_set).union(my_set).union({"JavaScript", "Python"}))
+
+print(my_new_set.difference(my_set))
+
+
+
 
 
 
---
+++ 07_dicts.py
@@ -0,0 +1,21 @@
+ #dicts#
+
+my_dict = dict()
+print(type(my_dict))
+
+my_other_dict = {"Nombre": "Manuel", "Apellido": "Gonzalez", "Edad": 26, "Altura": "1.74", 1:"Python"}
+my_dict = {
+    "Nombre": "Manuel",
+    "Apellido": "Gonzalez",
+    "Edad": 26,
+    "Altura": "1.74",
+    1:"Python"}
+print(my_other_dict)
+print(my_dict)
+# 5:17:21
+
+
+
+
+
+
