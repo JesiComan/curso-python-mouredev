d6e57b4719bb860522fcfd798015c6401118161d
JesiComan <jesicabelen.coman@gmail.com>
2024-02-10 13:47:21 -0300

    Clase 7 y 8, ty prácticas de Jesi

--- 07_dicts.py
+++ 07_dicts.py
@@ -12,7 +12,36 @@ my_dict = {
     1:"Python"}
 print(my_other_dict)
 print(my_dict)
-# 5:17:21
+
+print(len(my_other_dict))
+print(len(my_dict))
+
+print(my_dict["Nombre"])
+
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+print(my_other_dict.fromkeys(("Nombre", 1)))
+
+my_new_dict = my_other_dict.fromkeys(("Nombre", 1))
+print(my_new_dict) # Del Fromkeys Crea un diccionario nuevo, pero que no tiene valores
+
+my_values = my_new_dict.values()
+
+print(type(my_values))
+print(list(my_new_dict.values()))
+print(tuple(my_new_dict))
+print(set(my_new_dict))
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ 08_Conditionals.py
@@ -0,0 +1,39 @@
+### Condiciones ###
+
+my_condition = True
+
+if my_condition: # Es lo mismo que if my_condition == True:
+    print("Se ejecuta la condicion del if")
+
+print("La ejecucion continua")
+
+my_condition = 5 * 2
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 1:
+    print("Es igual a 1")
+else:
+  print("Es mayor que 10 y menor que 20")
+
+print("Es menor o igual que 10 o mayor o igual que 20")
+
+print("La ejecución continua")
+
+my_string = " "
+if my_string:
+    print("Mi cadena de texto no es vacía")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
